85bb44534ef63b57f083e6e34e460b13a1811754
Erik Albuquerque <erik.albuquerque.oficial@gmail.com>
2022-04-26 16:06:15 -0300

    feat: add fetch and list images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,29 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type Card = {
+  id: string;
+  ts: number;
+  title: string;
+  description: string;
+  url: string;
+};
+
+type fetchImagesType = {
+  data: Card[];
+  after: string | null;
+};
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({ pageParam = 0 }): Promise<fetchImagesType> => {
+    const response = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+    return response.data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,20 +38,22 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => lastPage.after,
+  });
 
   const formattedData = useMemo(() => {
-    // TODO FORMAT AND FLAT DATA ARRAY
+    let formattedPagesData: Card[] = [];
+    data?.pages.map(page => {
+      formattedPagesData = [...formattedPagesData, ...page.data];
+      // eslint-disable-next-line no-useless-return
+      return;
+    });
+    return formattedPagesData;
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
-
-  // TODO RENDER ERROR SCREEN
+  if (isLoading) return <Loading />;
+  if (isError) return <Error />;
 
   return (
     <>
@@ -37,7 +61,11 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt="1rem" onClick={() => fetchNextPage()}>
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
